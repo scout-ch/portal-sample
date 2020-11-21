588ff9d16296ea84f7ec2da384da598164620262
Vento <vento@pfadi-nuenenen.ch>
2020-11-21 22:32:35 +0100

    Add comments

--- index.php
+++ index.php
@@ -10,21 +10,30 @@ $title = 'Test';
 $content = 'Test';
 
 $array = [
+    // Tell the pbs-portal which users should see this notification.
+    // Enter at least one user-id. You can load it from the OAuth-Connection.
     'limitToUserIds' => [
       2,
     ],
+    // Tell the pbs-portal what title the notification should have.
+    // You need to pass the title in all four supported languages.
     'title' => [
         'de' => 'Midata Meldung: ',
         'en' => 'Midata message: ',
         'fr' => 'Message de Midata: ',
         'it' => 'Messaggio di Midata: ',
     ],
+    // Tell the pbs-portal what content the notification should have.
+    // You need to pass the content in all four supported languages.
     'content' => [
         'de' => 'Hallo!',
         'en' => 'Hello!',
         'fr' => 'Bonjour!',
         'it' => 'Ciao!',
     ],
+    // Tell the pbs-portal what URL the notification should point to.
+    // You need to pass the URL in all four supported languages.
+    // This attribute is optional.
     'url' => [
         'de' => '',
         'en' => 'Hello!',
